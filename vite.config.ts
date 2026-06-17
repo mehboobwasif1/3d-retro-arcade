e266a836dc7bd69134a96ec10251f6c2a2d2cdc1
Wasif Mehboob <mehboobwasif1@gmail.com>
2026-06-17 16:55:11 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/3d-retro-arcade/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
